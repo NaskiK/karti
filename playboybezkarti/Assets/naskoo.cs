4c78d0108d235e5356450be00596bb968c3df85c
NaskiK <nasko.2009@abv.bg>
2026-01-24 21:07:41 +0200

    Update naskoo.cs

--- playboybezkarti/Assets/naskoo.cs
+++ playboybezkarti/Assets/naskoo.cs
@@ -5,12 +5,13 @@ public class naskoo : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+//az sum
