50b8564e4eb54c12623895d17ac26e92f0d404fc
Nikinceto09 <nikolaichostoinov09@gmail.com>
2026-01-31 00:12:41 +0200

    Upgrade_shii

---
+++ playboybezkarti/Assets/Upgrades/Scripts/UpgradeButton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UpgradeButton : MonoBehaviour
+{
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI descriptionText;
+
+    Upgrade upgrade;
+
+    public void Setup(Upgrade newUpgrade)
+    {
+        upgrade = newUpgrade;
+        titleText.text = upgrade.upgradeName;
+        descriptionText.text = upgrade.description;
+    }
+
+    public void OnClick()
+    {
+        UpgradeManager.Instance.ChooseUpgrade(upgrade);
+    }
+}
